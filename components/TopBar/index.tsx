f8485360a6384db4816b70003f2b04cca26edfff
JustBecerra <justj.becerra@gmail.com>
2024-02-07 13:39:06 -0300

    add basic colors

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -5,7 +5,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 export const TopBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "#F7F8FA" }}>
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
             <MenuIcon />
