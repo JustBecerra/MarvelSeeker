3cb2707864329c84f47ff68703a8d0c09ce2a7ec
JustBecerra <justj.becerra@gmail.com>
2024-02-21 13:55:23 -0300

    add responsiveness to home card container

--- components/CardContainer/index.tsx
+++ components/CardContainer/index.tsx
@@ -64,7 +64,7 @@ export const CardContainer = () => {
         display: "flex",
         flex: 1,
         padding: 2,
-        flexDirection: "row",
+        flexDirection: { mobile: "column", laptop: "row" },
         overflowY: "auto",
         flexWrap: "wrap",
         gap: "2rem",
