76fc347e6c1326985ee0d4db6e919c9e200df811
JustBecerra <justj.becerra@gmail.com>
2024-02-21 16:09:58 -0300

    create structure for mobile and desktop design

---
+++ components/BarInteractionsMobile/index.tsx
@@ -0,0 +1,118 @@
+import {
+  Box,
+  IconButton,
+  Input,
+  InputAdornment,
+  Link,
+  useTheme,
+} from "@mui/material";
+import Image from "next/image";
+import marvelIcon from "../../public/marvel.svg";
+import SearchIcon from "@mui/icons-material/Search";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import StarIcon from "@mui/icons-material/Star";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import { ChangeEvent } from "react";
+type props = {
+  handleToggleMode: () => void;
+  handleFavorites: () => void;
+  handleChange: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  searchTerm: string;
+  showFavorites: boolean;
+};
+
+export const BarInteractionsMobile = ({
+  handleToggleMode,
+  handleFavorites,
+  handleChange,
+  searchTerm,
+  showFavorites,
+}: props) => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: "1rem",
+        p: "0.5rem",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ display: "flex", marginRight: "5%" }}>
+        <Link href={"/"}>
+          <Image priority src={marvelIcon} alt={""} width={72} height={36} />
+        </Link>
+        <IconButton
+          onClick={handleFavorites}
+          disableRipple
+          disableFocusRipple
+          disableTouchRipple
+          sx={{ marginRight: "1rem" }}
+        >
+          {!showFavorites ? (
+            <StarOutlineIcon
+              sx={{
+                display: "flex",
+                justifySelf: "flex-end",
+                width: "2.5rem",
+                height: "2.5rem",
+                fill: theme.palette.primary.dark,
+              }}
+            />
+          ) : (
+            <StarIcon
+              sx={{
+                display: "flex",
+                justifySelf: "flex-end",
+                width: "2.5rem",
+                height: "2.5rem",
+                fill: theme.palette.primary.dark,
+              }}
+            />
+          )}
+        </IconButton>
+
+        <IconButton
+          disableRipple
+          disableFocusRipple
+          disableTouchRipple
+          sx={{ ml: 1 }}
+          onClick={handleToggleMode}
+        >
+          {theme.palette.mode === "dark" ? (
+            <Brightness7Icon sx={{ fill: theme.palette.primary.dark }} />
+          ) : (
+            <Brightness4Icon sx={{ fill: theme.palette.primary.dark }} />
+          )}
+        </IconButton>
+      </Box>
+      <Input
+        placeholder="Buscar"
+        onChange={(e) => handleChange(e)}
+        value={searchTerm}
+        sx={{
+          "&.MuiInput-root::before": {
+            borderBottom: "none !important",
+          },
+        }}
+        startAdornment={
+          searchTerm ? (
+            <></>
+          ) : (
+            <InputAdornment position="start">
+              <SearchIcon
+                sx={{
+                  fill: theme.palette.primary.dark,
+                }}
+              />
+            </InputAdornment>
+          )
+        }
+      />
+    </Box>
+  );
+};
--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -1,21 +1,6 @@
 "use client";
-import {
-  AppBar,
-  Box,
-  IconButton,
-  Input,
-  InputAdornment,
-  Toolbar,
-  useTheme,
-} from "@mui/material";
-import marvelIcon from "../../public/marvel.svg";
-import SearchIcon from "@mui/icons-material/Search";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import StarIcon from "@mui/icons-material/Star";
-import Image from "next/image";
-import Divider from "@mui/material/Divider";
-import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import { AppBar, Box, Toolbar, useMediaQuery, useTheme } from "@mui/material";
+
 import { ChangeEvent, useState } from "react";
 import { useDispatch } from "react-redux";
 import {
@@ -24,15 +9,18 @@ import {
 } from "@/redux/features/character/character-slice";
 import { switchMode } from "@/redux/features/mode/mode-slice";
 import { useAppSelector } from "@/redux/store";
-import Link from "next/link";
+import { BarInteractionsDesktop } from "../BarInteractionsDesktop";
+import { BarInteractionsMobile } from "../BarInteractionsMobile";
 export const TopBar = () => {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
   const showFavorites = useAppSelector(
     (state) => state.charactersReducer.showFavorites
   );
-  const mode = useAppSelector((state) => state.modeReducer);
   const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("laptop"));
+  const mode = useAppSelector((state) => state.modeReducer);
+
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -62,114 +50,23 @@ export const TopBar = () => {
               width: "100%",
             }}
           >
-            <Box
-              sx={{
-                display: "flex",
-                gap: "1rem",
-                p: "0.5rem",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Link href={"/"}>
-                <Image
-                  priority
-                  src={marvelIcon}
-                  alt={""}
-                  width={72}
-                  height={36}
-                />
-              </Link>
-
-              <Divider
-                orientation="vertical"
-                variant="middle"
-                flexItem
-                sx={{
-                  borderRightWidth: 2.5,
-                  height: "2.5rem",
-                }}
+            {matches ? (
+              <BarInteractionsDesktop
+                handleToggleMode={handleToggleMode}
+                handleFavorites={handleFavorites}
+                handleChange={handleChange}
+                searchTerm={searchTerm}
+                showFavorites={showFavorites}
               />
-
-              <Input
-                placeholder="Buscar"
-                onChange={(e) => handleChange(e)}
-                value={searchTerm}
-                sx={{
-                  "&.MuiInput-root::before": {
-                    borderBottom: "none !important",
-                  },
-                }}
-                startAdornment={
-                  searchTerm ? (
-                    <></>
-                  ) : (
-                    <InputAdornment position="start">
-                      <SearchIcon
-                        sx={{
-                          fill: theme.palette.primary.dark,
-                        }}
-                      />
-                    </InputAdornment>
-                  )
-                }
+            ) : (
+              <BarInteractionsMobile
+                handleToggleMode={handleToggleMode}
+                handleFavorites={handleFavorites}
+                handleChange={handleChange}
+                searchTerm={searchTerm}
+                showFavorites={showFavorites}
               />
-            </Box>
-            <Box sx={{ display: "flex", marginRight: "5%" }}>
-              <IconButton
-                onClick={handleFavorites}
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                sx={{ marginRight: "1rem" }}
-              >
-                {!showFavorites ? (
-                  <StarOutlineIcon
-                    sx={{
-                      display: "flex",
-                      justifySelf: "flex-end",
-                      width: "2.5rem",
-                      height: "2.5rem",
-                      fill: theme.palette.primary.dark,
-                    }}
-                  />
-                ) : (
-                  <StarIcon
-                    sx={{
-                      display: "flex",
-                      justifySelf: "flex-end",
-                      width: "2.5rem",
-                      height: "2.5rem",
-                      fill: theme.palette.primary.dark,
-                    }}
-                  />
-                )}
-              </IconButton>
-
-              <Divider
-                orientation="vertical"
-                variant="middle"
-                flexItem
-                sx={{
-                  borderRightWidth: 2.5,
-                  height: "2.5rem",
-                }}
-              />
-
-              <IconButton
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                sx={{ ml: 1 }}
-                onClick={handleToggleMode}
-              >
-                {theme.palette.mode === "dark" ? (
-                  <Brightness7Icon sx={{ fill: theme.palette.primary.dark }} />
-                ) : (
-                  <Brightness4Icon sx={{ fill: theme.palette.primary.dark }} />
-                )}
-              </IconButton>
-            </Box>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
